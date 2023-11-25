7f79c8f283e5d03c9a014adebd856a2eeb827ebe
Tee <139244119+teeokh@users.noreply.github.com>
2023-11-25 17:52:46 +1000

    Added capability of crawling each individual page

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ def store_crawler(maxPages):
         soup = BeautifulSoup(source_text, "html.parser")
         for link in soup.findAll('a', {'class': 'ProductItem__ImageWrapper'}):
             href = "https://sweatscollective.com" + link.get('href')
-            print(href)
+            # print(href)
+            get_single_item_data(href)
         page += 1
+
+
+def get_single_item_data(item_url):
+    source_code = requests.get(item_url)
+    source_text = source_code.text
+    soup = BeautifulSoup(source_text, "html.parser")
+    # for item_name in soup.findAll('h1', {'class': 'ProductMeta__Title Heading u-h2'}):
+    # print(item_name.string)
+    for link in soup.findAll('a'):
+        href = "https://sweatscollective.com" + link.get('href')
+        print(href)
 
 
 store_crawler(1)
